f1225e7e27319406a3148d42720daab0565eb689
enzo-z <ezamoradev@gmail.com>
2019-06-24 04:29:22 -0300

    CREATED: index.html with his css and main script for the page!

--- Calculadora/main.js
+++ Calculadora/main.js
@@ -1,1 +1,9 @@
 //Javascript da calculadora!
+var btnElement = document.getElementById('botao');
+var n1 = parseInt(document.getElementById('number').value);
+var n2 = parseInt(document.getElementById('number2').value);
+
+console.log(n1+n2);
+btnElement.onclick = function(){
+    alert(n1+n2);
+}
