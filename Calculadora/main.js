a1ace919dbbab8f34266504e7a110272a8f27d5b
enzo-z <ezamoradev@gmail.com>
2019-06-24 22:18:05 -0300

    Tested the 'sum' operation in a raw prototype. Confident that I can create a JS Calculator now, and a sophisticated version of it!

--- Calculadora/main.js
+++ Calculadora/main.js
@@ -1,9 +1,8 @@
 //Javascript da calculadora!
 var btnElement = document.getElementById('botao');
-var n1 = parseInt(document.getElementById('number').value);
-var n2 = parseInt(document.getElementById('number2').value);
-
-console.log(n1+n2);
 btnElement.onclick = function(){
+	var n1 = parseInt(document.getElementById('number').value);
+	var n2 = parseInt(document.getElementById('number2').value);
     alert(n1+n2);
 }
+
